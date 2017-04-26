8dc201d3ebae2e49b9bc2e96cce3287be4c58576
Charlie Huguenard <charlie.huguenard@gmail.com>
2017-04-25 23:06:36 -0400

    a thing appears

---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour 
+{
+	public UnityEngine.UI.Image progressBar;
+	public UnityEngine.UI.Text text;
+
+	private const float _totalTime = 4 * 60 + 33;
+	private float _timeRemaining = _totalTime;
+
+	private void Update()
+	{
+		System.TimeSpan t = System.TimeSpan.FromSeconds(_timeRemaining);
+
+		text.text = string.Format("{0:D2}\'{1:D2}\"", 
+			t.Minutes, 
+			t.Seconds);
+
+		progressBar.fillAmount = 1.0f - _timeRemaining / _totalTime;
+
+		_timeRemaining -= Time.deltaTime;
+
+		if (_timeRemaining < 0.0f)
+		{
+			_timeRemaining = 0.0f;
+		}
+	}
+}
